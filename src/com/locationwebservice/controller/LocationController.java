196c792961efd008fcbd5795142dcdb9a665bca0
obruno <brunoalex_88@yahoo.com.br>
2016-06-14 17:23:59 -0300

    Adicionado método para inserir a localização e correção do nome do método getLocations que estava incorreto.

--- src/com/locationwebservice/controller/LocationController.java
+++ src/com/locationwebservice/controller/LocationController.java
@@ -7,8 +7,12 @@ import com.locationwebservice.model.Location;
 
 public class LocationController {
 
-	public ArrayList<Location> getocations(String deviceId) {
+	public ArrayList<Location> getLocations(String deviceId) {
 		return LocationDAO.getInstance().getLocations(deviceId);
+	}
+	
+	public int insertLocation(Location location) {
+		return LocationDAO.getInstance().insertLocation(location);
 	}
 	
 }
