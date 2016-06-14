60971ee9c19fca4c0aa8ca14bf198817da895807
obruno <brunoalex_88@yahoo.com.br>
2016-06-14 17:21:36 -0300

    Alteração do tipo de retorno do atributo time para String

--- src/com/locationwebservice/dao/LocationDAO.java
+++ src/com/locationwebservice/dao/LocationDAO.java
@@ -1,8 +1,10 @@
 package com.locationwebservice.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import com.locationwebservice.factory.ConnectionFactory;
@@ -20,12 +22,37 @@ public class LocationDAO extends ConnectionFactory {
 		
 	}
 	
+	public int insertLocation(Location location) {
+		Connection conn        = null;
+		PreparedStatement stmt = null;
+		String sql             = "INSERT INTO LOCATION VALUES (?, ?, ?, ?)";
+		
+		conn = getConnection();
+		
+		try {
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, location.getDeviceId());
+			stmt.setDouble(2, location.getLatitude());
+			stmt.setDouble(3, location.getLongitude());
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss");
+			stmt.setDate  (4, new Date(sdf.parse(location.getTime()).getTime()));
+			stmt.executeQuery();
+			return 0;
+		} catch (Exception e) {
+			System.out.println("Erro ao executar o insert no Oracle:");
+			e.printStackTrace();
+			return -1;
+		}
+		
+	}
+	
 	public ArrayList<Location> getLocations(String deviceId) {
 		Connection conn 	   		  = null;
 		PreparedStatement stmt 		  = null;
 		ResultSet rs 		   		  = null;
 		ArrayList<Location> locations = null;
-		String sql  		   		  = "SELECT LATITUDE, LONGITUDE, TIME FROM LOCATION WHERE DEVICE_ID = ?";
+		String sql  		   		  = "SELECT LATITUDE, LONGITUDE, TO_CHAR(TIME, 'DD/MM/YYYY HH24:MI:SS') TIME FROM LOCATION WHERE DEVICE_ID = ?";
 
 		conn 	  = getConnection();
 		locations = new ArrayList<Location>();
@@ -40,7 +67,7 @@ public class LocationDAO extends ConnectionFactory {
 				l.setDeviceId(deviceId);
 				l.setLatitude(rs.getDouble("LATITUDE"));
 				l.setLongitude(rs.getDouble("LONGITUDE"));
-				l.setTime(rs.getDate("TIME"));
+				l.setTime(rs.getString("TIME"));
 				
 				locations.add(l);
 				
