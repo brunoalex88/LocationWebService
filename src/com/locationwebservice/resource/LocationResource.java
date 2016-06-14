445bd5aca075c5c1c6e5f31e17b435beba65e48d
obruno <brunoalex_88@yahoo.com.br>
2016-06-14 17:24:32 -0300

    Adicionado método para inserir a localização via webservice.

--- src/com/locationwebservice/resource/LocationResource.java
+++ src/com/locationwebservice/resource/LocationResource.java
@@ -2,7 +2,9 @@ package com.locationwebservice.resource;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,11 +15,19 @@ import com.locationwebservice.model.Location;
 @Path("/location")
 public class LocationResource {
 	
+	@POST
+	@Path("/insert/{location}")
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public int insertLocation(@PathParam("location") Location location) {
+		return new LocationController().insertLocation(location);
+	}
+	
 	@GET
 	@Path("/getLocations/{deviceId}")
 	@Produces("application/json")
 	public ArrayList<Location> getLocations(@PathParam("deviceId") String deviceId) {
-		return new LocationController().getocations(deviceId);
+		return new LocationController().getLocations(deviceId);
 	}
 	
 }
